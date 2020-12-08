ac95f5e500f7e9f74f3f02e84ba19b87ad5ebc0c
Alexey Tkachuk <tkachuk.alexey@unicyb.kiev.ua>
2020-12-08 06:52:22 +0200

    Slots for UI objects

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,3 +13,79 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+bool MainWindow::eventFilter(QObject* watched, QEvent* event)
+{
+
+}
+
+void MainWindow::on_actionCrop_triggered()
+{
+
+}
+
+//Enter max window size
+void MainWindow::on_actionFullscreen_triggered()
+{
+    if(isFullScreen())
+        this->setWindowState(Qt::WindowMaximized);
+    else
+        this->setWindowState(Qt::WindowFullScreen);
+}
+
+void MainWindow::on_actionOpen_triggered()
+{
+
+}
+
+
+void MainWindow::on_actionPaintBlack_triggered()
+{
+
+}
+
+void MainWindow::on_actionRotateLeft_triggered()
+{
+
+}
+
+void MainWindow::on_actionRotateRight_triggered()
+{
+
+}
+
+void MainWindow::on_actionSave_triggered()
+{
+
+}
+
+void MainWindow::on_actionShowToolbar_triggered(bool checked)
+{
+
+}
+
+
+void MainWindow::on_actionUndo_triggered()
+{
+
+}
+
+void MainWindow::on_actionRedo_triggered()
+{
+
+}
+
+void MainWindow::on_actionZoomIn_triggered()
+{
+
+}
+
+void MainWindow::on_actionZoomOut_triggered()
+{
+
+}
+
+void MainWindow::on_actionZoomToFit_triggered()
+{
+
+}
+
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QVector>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -14,6 +15,23 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+private slots:
+    bool eventFilter(QObject* watched, QEvent* event);
+
+    void on_actionCrop_triggered();
+    void on_actionFullscreen_triggered();
+    void on_actionPaintBlack_triggered();
+    void on_actionOpen_triggered();
+    void on_actionRotateLeft_triggered();
+    void on_actionRotateRight_triggered();
+    void on_actionSave_triggered();
+    void on_actionShowToolbar_triggered(bool checked);
+    void on_actionUndo_triggered();
+    void on_actionRedo_triggered();
+    void on_actionZoomIn_triggered();
+    void on_actionZoomOut_triggered();
+    void on_actionZoomToFit_triggered();
+
 
 private:
     Ui::MainWindow *ui;
